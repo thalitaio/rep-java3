b8f66f31abd554faa5cc316fed1b2f8f3b84ea6f
Talita Rosa <talisrosa4@gmail.com>
2021-09-28 16:25:54 -0300

    adição do cabeçalho

---
+++ ATP14.java
@@ -0,0 +1,23 @@
+/*
+_ Construa um método que imprima uma mensagem para ser usada como cabeçalho da aplicação. A mensagem deve exibir a mensagem “Calculadora”.
+_ Construa um método que imprima o menu de opções contendo as opções somar, subtratir, multiplicar e dividir.
+_ Construa um método que solicite ao usuario que ele digite uma das opções do menu, armazene a opção em uma variável, valide o 
+valor e retorne apenas quando uma opção válida for digitada.
+_ Crie um método que receba a opção escolhida pelo usuário e encaminhe para o método que irá realizar a operação correta, seja de soma, subtração, multiplicação e divisão.
+_ Construa os metodos de soma,  subtração, multiplicação e divisão.Todos os métodos devem receber dois parâmetros double realizar 
+a operação e retornar o valor do cálculo.
+_ Imprima o resultado das operações fora do metodo que realiza o cálculo.
+
+*/
+
+
+public class ATP14 {
+    public static void main(String[] args) {
+        cabecalho();
+    }
+
+    static void cabecalho(){
+        System.out.println("===== Caculadora =====");
+    } 
+}
+
